ebc0dbc1fd73313797daf03264f7574848d37216
FhRh <yiyop@naver.com>
2025-12-17 19:54:24 +0900

    mariadb 의존성 제거

--- wabi/build.gradle.kts
+++ wabi/build.gradle.kts
@@ -57,7 +57,6 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
-    runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
     runtimeOnly("com.mysql:mysql-connector-j")
 }
 
