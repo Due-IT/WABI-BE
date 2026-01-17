c338ba000021b5901ef592b71770597f1d0804e9
FhRh <yiyop@naver.com>
2026-01-17 15:59:04 +0900

    wabi-attendance-consumer 프로젝트 생성

---
+++ wabi-attendance-consumer/src/main/kotlin/wabi/wabi_attendance_consumer/WabiAttendanceConsumerApplication.kt
@@ -0,0 +1,11 @@
+package wabi.wabi_attendance_consumer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WabiAttendanceConsumerApplication
+
+fun main(args: Array<String>) {
+	runApplication<WabiAttendanceConsumerApplication>(*args)
+}
